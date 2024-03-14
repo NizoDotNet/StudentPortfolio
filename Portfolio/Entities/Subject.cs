535b69710b11e14896cbf2a63fe549cf0ff47217
NizoDotNet <mmmdovnizami.official@gmail.com>
2024-03-14 20:48:15 +0400

    fixed bug?

--- Portfolio/Entities/Subject.cs
+++ Portfolio/Entities/Subject.cs
@@ -11,8 +11,8 @@ public class Subject
     [StringLength(25)]
     public string Name { get; set; }
 
-    public IList<LabWork> LabWorks { get; set; } = [];
-    public IList<Class> Classes { get; set; } = [];
+    public IList<LabWork> LabWorks { get; set; }
+    public IList<Class> Classes { get; set; } 
 
     public string TeacherId { get; set; }
     public AppUser Teacher { get; set; }
--- Portfolio/Models/LabWork/AddLabWorkDto.cs
+++ Portfolio/Models/LabWork/AddLabWorkDto.cs
@@ -7,5 +7,4 @@ public class AddLabWorkDto
     [MinLength(3)]
     public string Name { get; set; }
     public bool Completed { get; set; } = false;
-    public int SubjectId { get; set; }
 }
